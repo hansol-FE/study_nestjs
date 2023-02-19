320c20c6eae0666488d85a1e742588db5bc0f737
hansol <dundunki@naver.com>
2023-02-19 17:47:51 +0900

    feat: Dependency Injection

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  /*  접근 제한자(public, protected,private)을 생성자(constructor) 파라미터에 선언하면,
+    접근 제한자가 사용된 파라미터는 암묵적으로 클래스 프로퍼티로 선언된다.
+
+    기존 프로퍼티 사용 예시)
+    boardsService: BoardsService;
+
+    constructor(boardsService:BoardsService) {
+        this.boardsService = boardsService;
+    }
+ */
+  constructor(private boardService: BoardsService) {}
+}
