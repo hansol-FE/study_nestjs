773ade7ab55899e9a7baa3d8e4ed92786307e31b
hansolpark <dundunki@naver.com>
2023-02-24 10:41:01 +0900

    add: delelte  board by Id

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-bord.dto';
@@ -30,5 +30,10 @@ export class BoardsController {
   @Get('/:id')
   getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    return this.boardsService.deleteBoardById(id);
   }
 }
